296c2f07e8977c51a09f65c58367c3c7125b7aa2
Alain Igban <apigban@gmail.com>
2022-12-02 01:34:15 +0400

    Refactor - moved terraform files 1 dir up

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,8 +6,8 @@ resource "local_file" "tf_one_AnsibleInventory" {
       server-private-ipv4 = oci_core_instance.public.private_ip
       server-public-ipv4  = oci_core_instance.public.public_ip
 
-      server-disk-id   = oci_core_volume.disk-one.id
-      server-disk-name = oci_core_volume.disk-one.display_name
+      server-disk-id      = oci_core_volume.disk-one.id
+      server-disk-name    = oci_core_volume.disk-one.display_name
       server-privkey-path = "${oci_core_instance.public.display_name}.yaml"
     }
 
