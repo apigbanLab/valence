31193ca9114ea4a0fdca23b1aa2c7669401d525a
Alain Igban <apigban@gmail.com>
2022-12-02 01:11:58 +0400

    Added privatekey path to host_vars template
    created templates directory

--- terraform/mainnet/outputs.tf
+++ terraform/mainnet/outputs.tf
@@ -1,6 +1,6 @@
-# Export Terraform variable values to an Ansible var_file
+# Export Terraform variable values to an Ansible host_vars file
 resource "local_file" "tf_one_AnsibleInventory" {
-  content = templatefile("inventory.tmpl",
+  content = templatefile("templates/host_vars.tmpl",
     {
       server-name         = oci_core_instance.public.display_name
       server-private-ipv4 = oci_core_instance.public.private_ip
@@ -8,9 +8,9 @@ resource "local_file" "tf_one_AnsibleInventory" {
 
       server-disk-id   = oci_core_volume.disk-one.id
       server-disk-name = oci_core_volume.disk-one.display_name
-
+      server-privkey-path = "${oci_core_instance.public.display_name}.yaml"
     }
 
   )
-  filename = "../../server-config/host_vars/one.yaml"
+  filename = "../../server-config/host_vars/${oci_core_instance.public.display_name}.yaml"
 }
