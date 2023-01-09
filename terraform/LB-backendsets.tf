6c50fbe9c7a6813c2c5926429c79816d596d3aeb
Alain Igban <apigban@gmail.com>
2023-01-09 19:21:50 +0400

    Removed UDP Backends and Listeners
    Vxlan using wireguard and Flannel IPv4 needs direct UDP to nodes.

--- terraform/LB-backendsets.tf
+++ terraform/LB-backendsets.tf
@@ -14,18 +14,18 @@ resource "oci_network_load_balancer_backend_set" "k3sserver-ILB-BackendSet_HTTPS
   policy                   = var.k3sserver-ILB_BackendSet_policy
 }
 
-resource "oci_network_load_balancer_backend_set" "k3sserver-ILB-BackendSet_UDP8472" {
-  health_checker {
-    protocol           = var.k3sserver-ILB_Listeners.wireguardvxlan.protocol
-    interval_in_millis = 10000
-    port               = var.k3sserver-ILB_Listeners.wireguardvxlan.port
-    retries            = 3
-    timeout_in_millis  = 2000
-  }
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sserver-ILB.id
-  name                     = var.k3sserver-ILB_Listeners.wireguardvxlan.display_name
-  policy                   = var.k3sserver-ILB_BackendSet_policy
-}
+# resource "oci_network_load_balancer_backend_set" "k3sserver-ILB-BackendSet_UDP8472" {
+#   health_checker {
+#     protocol           = var.k3sserver-ILB_Listeners.wireguardvxlan.protocol
+#     interval_in_millis = 10000
+#     port               = var.k3sserver-ILB_Listeners.wireguardvxlan.port
+#     retries            = 3
+#     timeout_in_millis  = 2000
+#   }
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sserver-ILB.id
+#   name                     = var.k3sserver-ILB_Listeners.wireguardvxlan.display_name
+#   policy                   = var.k3sserver-ILB_BackendSet_policy
+# }
 
 resource "oci_network_load_balancer_backend_set" "k3sserver-ILB-BackendSet_TCP10250" {
   health_checker {
@@ -40,18 +40,18 @@ resource "oci_network_load_balancer_backend_set" "k3sserver-ILB-BackendSet_TCP10
   policy                   = var.k3sserver-ILB_BackendSet_policy
 }
 
-resource "oci_network_load_balancer_backend_set" "k3sserver-ILB-BackendSet_UDP51820" {
-  health_checker {
-    protocol           = var.k3sserver-ILB_Listeners.wireguardflannel.protocol
-    interval_in_millis = 10000
-    port               = var.k3sserver-ILB_Listeners.wireguardflannel.port
-    retries            = 3
-    timeout_in_millis  = 2000
-  }
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sserver-ILB.id
-  name                     = var.k3sserver-ILB_Listeners.wireguardflannel.display_name
-  policy                   = var.k3sserver-ILB_BackendSet_policy
-}
+# resource "oci_network_load_balancer_backend_set" "k3sserver-ILB-BackendSet_UDP51820" {
+#   health_checker {
+#     protocol           = var.k3sserver-ILB_Listeners.wireguardflannel.protocol
+#     interval_in_millis = 10000
+#     port               = var.k3sserver-ILB_Listeners.wireguardflannel.port
+#     retries            = 3
+#     timeout_in_millis  = 2000
+#   }
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sserver-ILB.id
+#   name                     = var.k3sserver-ILB_Listeners.wireguardflannel.display_name
+#   policy                   = var.k3sserver-ILB_BackendSet_policy
+# }
 
 #K3s Agent Backend Sets
 resource "oci_network_load_balancer_backend_set" "k3sagent-ILB-BackendSet_HTTPS6443" {
@@ -69,31 +69,31 @@ resource "oci_network_load_balancer_backend_set" "k3sagent-ILB-BackendSet_HTTPS6
   policy                   = var.k3sagent-ILB_BackendSet_policy
 }
 
-resource "oci_network_load_balancer_backend_set" "k3sagent-ILB-BackendSet_UDP51820" {
-  health_checker {
-    protocol           = var.k3sagent-ILB_Listeners.wireguardflannel.protocol
-    interval_in_millis = 10000
-    port               = var.k3sagent-ILB_Listeners.wireguardflannel.port
-    retries            = 3
-    timeout_in_millis  = 2000
-  }
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sagent-ILB.id
-  name                     = var.k3sagent-ILB_Listeners.wireguardflannel.display_name
-  policy                   = var.k3sagent-ILB_BackendSet_policy
-}
+# resource "oci_network_load_balancer_backend_set" "k3sagent-ILB-BackendSet_UDP51820" {
+#   health_checker {
+#     protocol           = var.k3sagent-ILB_Listeners.wireguardflannel.protocol
+#     interval_in_millis = 10000
+#     port               = var.k3sagent-ILB_Listeners.wireguardflannel.port
+#     retries            = 3
+#     timeout_in_millis  = 2000
+#   }
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sagent-ILB.id
+#   name                     = var.k3sagent-ILB_Listeners.wireguardflannel.display_name
+#   policy                   = var.k3sagent-ILB_BackendSet_policy
+# }
 
-resource "oci_network_load_balancer_backend_set" "k3sagent-ILB-BackendSet_UDP8472" {
-  health_checker {
-    protocol           = var.k3sagent-ILB_Listeners.wireguardvxlan.protocol
-    interval_in_millis = 10000
-    port               = var.k3sagent-ILB_Listeners.wireguardvxlan.port
-    retries            = 3
-    timeout_in_millis  = 2000
-  }
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sagent-ILB.id
-  name                     = var.k3sagent-ILB_Listeners.wireguardvxlan.display_name
-  policy                   = var.k3sagent-ILB_BackendSet_policy
-}
+# resource "oci_network_load_balancer_backend_set" "k3sagent-ILB-BackendSet_UDP8472" {
+#   health_checker {
+#     protocol           = var.k3sagent-ILB_Listeners.wireguardvxlan.protocol
+#     interval_in_millis = 10000
+#     port               = var.k3sagent-ILB_Listeners.wireguardvxlan.port
+#     retries            = 3
+#     timeout_in_millis  = 2000
+#   }
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sagent-ILB.id
+#   name                     = var.k3sagent-ILB_Listeners.wireguardvxlan.display_name
+#   policy                   = var.k3sagent-ILB_BackendSet_policy
+# }
 
 resource "oci_network_load_balancer_backend_set" "k3sagent-ILB-BackendSet_TCP10250" {
   health_checker {
--- terraform/LB-listener-dns.tf
+++ terraform/LB-listener-dns.tf
@@ -7,13 +7,7 @@ resource "oci_network_load_balancer_listener" "k3sserver-ILB_Listener_HTTPS6443"
   protocol                 = var.k3sserver-ILB_Listeners.kubeapiserver.protocol
 }
 
-resource "oci_network_load_balancer_listener" "k3sserver-ILB_Listener_UDP8472" {
-  default_backend_set_name = oci_network_load_balancer_backend_set.k3sserver-ILB-BackendSet_UDP8472.name
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sserver-ILB.id
-  name                     = var.k3sserver-ILB_Listeners.wireguardvxlan.display_name
-  port                     = var.k3sserver-ILB_Listeners.wireguardvxlan.port
-  protocol                 = var.k3sserver-ILB_Listeners.wireguardvxlan.protocol
-}
+
 
 resource "oci_network_load_balancer_listener" "k3sserver-ILB_Listener_TCP10250" {
   default_backend_set_name = oci_network_load_balancer_backend_set.k3sserver-ILB-BackendSet_TCP10250.name
@@ -23,13 +17,13 @@ resource "oci_network_load_balancer_listener" "k3sserver-ILB_Listener_TCP10250" 
   protocol                 = var.k3sserver-ILB_Listeners.metrics.protocol
 }
 
-resource "oci_network_load_balancer_listener" "k3sserver-ILB_Listener_UDP51820" {
-  default_backend_set_name = oci_network_load_balancer_backend_set.k3sserver-ILB-BackendSet_UDP51820.name
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sserver-ILB.id
-  name                     = var.k3sserver-ILB_Listeners.wireguardflannel.display_name
-  port                     = var.k3sserver-ILB_Listeners.wireguardflannel.port
-  protocol                 = var.k3sserver-ILB_Listeners.wireguardflannel.protocol
-}
+# resource "oci_network_load_balancer_listener" "k3sserver-ILB_Listener_UDP51820" {
+#   default_backend_set_name = oci_network_load_balancer_backend_set.k3sserver-ILB-BackendSet_UDP51820.name
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sserver-ILB.id
+#   name                     = var.k3sserver-ILB_Listeners.wireguardflannel.display_name
+#   port                     = var.k3sserver-ILB_Listeners.wireguardflannel.port
+#   protocol                 = var.k3sserver-ILB_Listeners.wireguardflannel.protocol
+# }
 
 #K3s Agent Listeners
 # resource "oci_network_load_balancer_listener" "k3sagent-ILB_Listener_80" {
@@ -56,13 +50,13 @@ resource "oci_network_load_balancer_listener" "k3sagent-ILB_Listener_HTTPS6443" 
   protocol                 = var.k3sagent-ILB_Listeners.kubeapiserver.protocol
 }
 
-resource "oci_network_load_balancer_listener" "k3sagent-ILB_Listener_8472" {
-  default_backend_set_name = oci_network_load_balancer_backend_set.k3sagent-ILB-BackendSet_UDP8472.name
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sagent-ILB.id
-  name                     = var.k3sagent-ILB_Listeners.wireguardvxlan.display_name
-  port                     = var.k3sagent-ILB_Listeners.wireguardvxlan.port
-  protocol                 = var.k3sagent-ILB_Listeners.wireguardvxlan.protocol
-}
+# resource "oci_network_load_balancer_listener" "k3sagent-ILB_Listener_8472" {
+#   default_backend_set_name = oci_network_load_balancer_backend_set.k3sagent-ILB-BackendSet_UDP8472.name
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sagent-ILB.id
+#   name                     = var.k3sagent-ILB_Listeners.wireguardvxlan.display_name
+#   port                     = var.k3sagent-ILB_Listeners.wireguardvxlan.port
+#   protocol                 = var.k3sagent-ILB_Listeners.wireguardvxlan.protocol
+# }
 
 resource "oci_network_load_balancer_listener" "k3sagent-ILB_Listener_TCP10250" {
   default_backend_set_name = oci_network_load_balancer_backend_set.k3sagent-ILB-BackendSet_TCP10250.name
@@ -72,13 +66,13 @@ resource "oci_network_load_balancer_listener" "k3sagent-ILB_Listener_TCP10250" {
   protocol                 = var.k3sagent-ILB_Listeners.metrics.protocol
 }
 
-resource "oci_network_load_balancer_listener" "k3sagent-ILB_Listener_UDP51820" {
-  default_backend_set_name = oci_network_load_balancer_backend_set.k3sagent-ILB-BackendSet_UDP51820.name
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sagent-ILB.id
-  name                     = var.k3sagent-ILB_Listeners.wireguardflannel.display_name
-  port                     = var.k3sagent-ILB_Listeners.wireguardflannel.port
-  protocol                 = var.k3sagent-ILB_Listeners.wireguardflannel.protocol
-}
+# resource "oci_network_load_balancer_listener" "k3sagent-ILB_Listener_UDP51820" {
+#   default_backend_set_name = oci_network_load_balancer_backend_set.k3sagent-ILB-BackendSet_UDP51820.name
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.k3sagent-ILB.id
+#   name                     = var.k3sagent-ILB_Listeners.wireguardflannel.display_name
+#   port                     = var.k3sagent-ILB_Listeners.wireguardflannel.port
+#   protocol                 = var.k3sagent-ILB_Listeners.wireguardflannel.protocol
+# }
 
 #K3s DB Listeners
 resource "oci_network_load_balancer_listener" "k3sdb-ILB_Listener_TCP5432" {
