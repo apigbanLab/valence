a1786c9443e0daa1d6c84d71b704c2dbfbd28396
Alain Igban <apigban@gmail.com>
2023-01-27 15:49:45 +0400

    added unique metadata.
    Terraform doesn't replace the file if it has the same name
    
    unique metadata (md5) ensures that the file will be changed on execution of terraform apply.

--- terraform/persist_files.tf
+++ terraform/persist_files.tf
@@ -3,4 +3,7 @@ resource "oci_objectstorage_object" "privkey_upload" {
     source = "${path.cwd}/oci.privkey"
     namespace = "axafm68tcvts"
     object = "valence/oci.privkey"
+    metadata = {
+        "md5": "${filemd5("${path.cwd}/oci.privkey")}"
+    }
 }
